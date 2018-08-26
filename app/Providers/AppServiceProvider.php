76387c6a63b7877b856174b74077e50032e304d6
Lucas Rolff <lucas@lucasrolff.com>
2018-08-26 15:44:14 +0200

    Force https if production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
     }
 
