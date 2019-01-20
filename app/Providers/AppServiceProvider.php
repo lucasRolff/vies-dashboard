fbdb55cbf5e0b4626f428f6ad60d571bb95534ed
Laravel Shift <shift@laravelshift.com>
2019-01-20 06:49:24 +0000

    Adopt PSR-2 coding style
    The Laravel framework adopts the PSR-2 coding style in version 5.1.
    Laravel apps *should* adopt this coding style as well. Read the
    [PSR-2 coding style guide][1] for more details and check out [PHPCS][2]
    to use as a code formatting tool.
    
    [1]: https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-2-coding-style-guide.md
    [2]: https://github.com/squizlabs/PHP_CodeSniffer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->environment('production')) {
+        if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
         Schema::defaultStringLength(191);
